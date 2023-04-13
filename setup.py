2b2f161d71f0a499697d909ff6094a0b63fc3f53
Rafaella Christe Corrêa <rafaella.c@sidi.org.br>
2023-04-13 07:25:08 -0300

    Adding code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning system end-to-end using IMDB Dataset to build a movie recommendation system',
+    author='Rafaella Christe Corrêa',
+    license='MIT',
+)
